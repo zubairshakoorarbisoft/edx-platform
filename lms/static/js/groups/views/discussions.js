a13261a736f98c020596aada05c35f5a66112f07
cahrens <christina@edx.org>
2017-05-13 21:46:49 -0400

    Very beginning of radio button work.

--- lms/static/js/groups/views/discussions.js
+++ lms/static/js/groups/views/discussions.js
@@ -9,6 +9,9 @@
             var hiddenClass = 'is-hidden';
 
             var DiscussionsView = Backbone.View.extend({
+                events: {
+                    'change .division-scheme': 'divisionSchemeChanged'
+                },
 
                 initialize: function(options) {
                     this.template = HtmlUtils.template($('#discussions-tpl').text());
@@ -17,10 +20,46 @@
                 },
 
                 render: function() {
-                    HtmlUtils.setHtml(this.$el, this.template({}));
-
+                    HtmlUtils.setHtml(this.$el, this.template({availableSchemes: this.getDivisionSchemeData()}));
+                    this.divisionSchemeChanged();
                     this.showDiscussionTopics();
                     return this;
+                },
+
+                getDivisionSchemeData: function() {
+                    // TODO: get available schemes and currently selected scheme from this.discussionSettings
+                    return [
+                        {
+                            key: 'none',
+                            displayName: gettext('None'),
+                            descriptiveText: gettext('All discussions are unified'),
+                            selected: false
+                        },
+                        {
+                            key: 'enrollment_track',
+                            displayName: gettext('Enrollment Track'),
+                            descriptiveText: gettext('Divide selected discussions by enrollment track'),
+                            selected: false
+                        },
+                        {
+                            key: 'cohort',
+                            displayName: gettext('Cohort'),
+                            descriptiveText: gettext('Divide selected discussions by cohort'),
+                            selected: true
+                        }
+
+                    ];
+                },
+
+                divisionSchemeChanged: function() {
+                    var selectedScheme = this.$('input[name="division-scheme"]:checked').val(),
+                        topicNav = this.$('.topic-division-nav');
+
+                    if (selectedScheme === 'none') {
+                        topicNav.addClass(hiddenClass);
+                    } else {
+                        topicNav.removeClass(hiddenClass);
+                    }
                 },
 
                 getSectionCss: function(section) {
@@ -28,7 +67,7 @@
                 },
 
                 showDiscussionTopics: function() {
-                    var dividedDiscussionsElement = this.$('.discussions-nav').removeClass(hiddenClass);
+                    var dividedDiscussionsElement = this.$('.discussions-nav');
                     if (!this.CourseWideDiscussionsView) {
                         this.CourseWideDiscussionsView = new CourseWideDiscussionsView({
                             el: dividedDiscussionsElement,
