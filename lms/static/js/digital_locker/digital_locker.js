7116fdddd7af82b5cf162e7830fc2e7b17d72d7a
Michael Frey <mfrey@edx.org>
2017-09-27 15:07:35 -0400

    more style

--- lms/static/js/digital_locker/digital_locker.js
+++ lms/static/js/digital_locker/digital_locker.js
@@ -1,10 +1,18 @@
+var is_showing = false;
+
 $('.digital-locker-dropdown').on('click', function(e) {
     // Only allow one file viewer at a time
     $('.file-viewer-container').hide()
 
     // Show file view
     $fileViewerContainer = $(e.target).parent().find('.file-viewer-container');
-    $fileViewerContainer.show();
+    if (is_showing) {
+        $fileViewerContainer.hide();
+        is_showing = false;
+    } else {
+        $fileViewerContainer.show();
+        is_showing = true;
+    }
 });
 
 $('.file-viewer-container .close').on('click', function(e) {
