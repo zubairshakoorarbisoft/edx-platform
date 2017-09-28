b3b5b6e6a1a3de3959b033d4b624dde21f691786
Michael Frey <mfrey@edx.org>
2017-09-28 11:26:47 -0400

    fixes

--- cms/static/js/factories/asset_index.js
+++ cms/static/js/factories/asset_index.js
@@ -2,6 +2,14 @@ define([
     'jquery', 'js/collections/asset', 'js/views/assets', 'jquery.fileupload'
 ], function($, AssetCollection, AssetsView) {
     'use strict';
+    $('.upload-modal .close-button').on('click', function() {
+        var $iframe = $('.digital-locker iframe');
+        if (!$iframe) {
+            document.querySelector('.digital-locker iframe').contentWindow.location.reload();
+        } else {
+            document.location.reload();
+        }
+    });
     return function(config) {
         var assets = new AssetCollection(),
             assetsView;
