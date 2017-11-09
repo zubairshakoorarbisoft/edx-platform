f731a1cbbcaa26a2fba412d08d1e5d83712b09b1
noraiz-anwar <noraizbhatti@gmail.com>
2017-11-09 18:01:23 +0500

    management command overidden fro test on sandbox

--- cms/djangoapps/contentstore/management/commands/clone_course.py
+++ cms/djangoapps/contentstore/management/commands/clone_course.py
@@ -9,40 +9,50 @@ from opaque_keys.edx.keys import CourseKey
 from student.roles import CourseInstructorRole, CourseStaffRole
 from xmodule.modulestore import ModuleStoreEnum
 from xmodule.modulestore.django import modulestore
-
-
+from django.contrib.auth.models import User
+from xmodule.modulestore.xml_importer import import_library_from_xml
+from xmodule.contentstore.django import contentstore
+from contentstore.utils import add_instructor, reverse_library_url
 #
 # To run from command line: ./manage.py cms clone_course --settings=dev master/300/cough edx/111/foo
 #
 class Command(BaseCommand):
     """
-    Clone a MongoDB-backed course to another location
+    Create 600 libraries
     """
     help = 'Clone a MongoDB backed course to another location'
-
-    def add_arguments(self, parser):
-        parser.add_argument('source_course_id', help='Course ID to copy from')
-        parser.add_argument('dest_course_id', help='Course ID to copy to')
 
     def handle(self, *args, **options):
         """
         Execute the command
         """
 
-        source_course_id = CourseKey.from_string(options['source_course_id'])
-        dest_course_id = CourseKey.from_string(options['dest_course_id'])
+        dirpath = u'library'
+        LIBRARY_ROOT = 'library.xml'
+        root_name = LIBRARY_ROOT
+        import_func = import_library_from_xml
 
-        mstore = modulestore()
+        user = User.objects.get(username='honor')
+        store = modulestore()
 
-        print("Cloning course {0} to {1}".format(source_course_id, dest_course_id))
 
-        with mstore.bulk_operations(dest_course_id):
-            if mstore.clone_course(source_course_id, dest_course_id, ModuleStoreEnum.UserID.mgmt_command):
-                print("copying User permissions...")
-                # purposely avoids auth.add_user b/c it doesn't have a caller to authorize
-                CourseInstructorRole(dest_course_id).add_users(
-                    *CourseInstructorRole(source_course_id).users_with_role()
+        for i in range(1,600):
+            with store.default_store(ModuleStoreEnum.Type.split):
+                new_lib = store.create_library(
+                    org='Arbisoft',
+                    library='library{}'.format(i),
+                    user_id=user.id,
+                    fields={"display_name": 'New Library'},
                 )
-                CourseStaffRole(dest_course_id).add_users(
-                    *CourseStaffRole(source_course_id).users_with_role()
-                )
+            # Give the user admin ("Instructor") role for this library:
+            add_instructor(new_lib.location.library_key, user, user)
+
+            courselike_module = modulestore().get_library(new_lib.location.library_key)
+
+            courselike_items = import_func(
+                modulestore(), user.id,
+                u'/edx/app/edxapp/edx-platform', [dirpath],
+                load_error_modules=False,
+                static_content_store=contentstore(),
+                target_id=new_lib.location.library_key
+            )
