106c0e25b8291b8488c788225fe9b7a289c8de92
Bessie Steinberg <bsteinberg@edx.org>
2017-11-27 15:42:40 -0500

    add locks to subsections that need prereqs agnostic of whether the prereq is met

--- openedx/features/course_experience/views/course_outline.py
+++ openedx/features/course_experience/views/course_outline.py
@@ -11,7 +11,7 @@ from courseware.courses import get_course_overview_with_access
 from openedx.core.djangoapps.plugin_api.views import EdxFragmentView
 
 from ..utils import get_course_outline_block_tree
-from util.milestones_helpers import get_course_content_milestones, get_course_content_milestones_by_course
+from util.milestones_helpers import get_course_content_milestones_by_course, milestones_achieved_by_user
 
 
 class CourseOutlineFragmentView(EdxFragmentView):
@@ -25,22 +25,24 @@ class CourseOutlineFragmentView(EdxFragmentView):
         """
         course_key = CourseKey.from_string(course_id)
         course_overview = get_course_overview_with_access(request.user, 'load', course_key, check_if_enrolled=True)
-
         course_block_tree = get_course_outline_block_tree(request, course_id)
-
-
 
         if not course_block_tree:
             return None
+
+        completed_milestones = milestones_achieved_by_user(request.user, course_id)
+
 
         milestones = get_course_content_milestones_by_course(
             course_id=course_key,
             relationship='requires',
             user_id=request.user.id)
 
-        content_block_milestones = []
+        content_block_milestones = {}
         for milestone in milestones:
-            content_block_milestones.append( milestone['content_id'] )
+            content_block_milestones[ milestone['content_id'] ] = {
+                'completed_prereqs': False
+            }
 
         context = {
             'csrf': csrf(request)['csrf_token'],
