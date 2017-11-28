d38337ed83446d3407f3e90691e4a70d13a8d670
Bessie Steinberg <bsteinberg@edx.org>
2017-11-28 13:12:23 -0500

    show which prereq is needed

--- openedx/features/course_experience/views/course_outline.py
+++ openedx/features/course_experience/views/course_outline.py
@@ -6,11 +6,11 @@ from django.template.loader import render_to_string
 from opaque_keys.edx.keys import CourseKey
 from web_fragments.fragment import Fragment
 
-
+from lms.djangoapps.course_api.blocks.api import get_blocks
 from courseware.courses import get_course_overview_with_access
 from openedx.core.djangoapps.plugin_api.views import EdxFragmentView
 
-from ..utils import get_course_outline_block_tree
+from ..utils import get_course_outline_block_tree, get_all_course_blocks
 from util.milestones_helpers import get_all_course_content_milestones, get_course_content_milestones_by_course, milestones_achieved_by_user, get_course_content_milestones
 
 
@@ -26,6 +26,7 @@ class CourseOutlineFragmentView(EdxFragmentView):
         course_key = CourseKey.from_string(course_id)
         course_overview = get_course_overview_with_access(request.user, 'load', course_key, check_if_enrolled=True)
         course_block_tree = get_course_outline_block_tree(request, course_id)
+        all_course_blocks = get_all_course_blocks(request, course_id)
 
         if not course_block_tree:
             return None
@@ -33,6 +34,7 @@ class CourseOutlineFragmentView(EdxFragmentView):
 
         content_milestones = {}
         
+        #TODO: should i be using a user id here? probably
         course_prereqs = get_all_course_content_milestones(course_key) #, relationship='requires')
 
         unfulfilled_prereqs = get_course_content_milestones_by_course(
@@ -49,7 +51,9 @@ class CourseOutlineFragmentView(EdxFragmentView):
                 }
         
         for milestone in unfulfilled_prereqs:
-            content_milestones[ milestone['content_id'] ]['completed_prereqs'] = False
+            content_milestones[milestone['content_id']]['completed_prereqs'] = False
+            content_milestones[milestone['content_id']]['prereq'] = all_course_blocks['blocks'][milestone['namespace'].replace('.gating','')]['display_name']
+            #TODO: can there be multiple prereqs?
 
         context = {
             'csrf': csrf(request)['csrf_token'],
