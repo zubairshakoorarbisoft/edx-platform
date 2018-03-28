9278301630ac0e1862f7b7b4543982dc28e9967b
M. Rehan <muhammadrehan69@gmail.com>
2018-03-28 16:20:14 +0500

    Revert "Mrehan/remove subs from metadata"

--- common/test/acceptance/pages/studio/video/video.py
+++ common/test/acceptance/pages/studio/video/video.py
@@ -64,6 +64,7 @@ DEFAULT_SETTINGS = [
 
     # advanced
     [DISPLAY_NAME, 'Video', False],
+    ['Default Timed Transcript', '', False],
     ['Download Transcript Allowed', 'False', False],
     ['Downloadable Transcript URL', '', False],
     ['Show Transcript', 'True', False],
