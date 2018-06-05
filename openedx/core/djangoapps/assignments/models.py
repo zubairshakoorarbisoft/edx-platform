53f678418f9d373d22b180a50b6952b2207c44bf
Jason Myatt <jmyatt@edx.org>
2018-06-05 15:51:29 -0400

    add models

--- openedx/core/djangoapps/assignments/models.py
+++ openedx/core/djangoapps/assignments/models.py
@@ -3,4 +3,21 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class CourseRunAssignment(models.Model):
+    """
+    CourseRunAssignment model.
+    """
+    course_run_id = models.CharField(max_length=100, null=False, blank=False)
+    sequential_id = models.CharField(max_length=100, null=False, blank=False)
+    sequential_name = models.CharField(max_length=255, null=False, blank=False)
+    due_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta(object):
+        """
+        CourseRunAssignment metadata.
+        """
+        unique_together = ('course_run_id', 'sequential_id')
+
+    def __unicode__(self):
+        return self.sequential_name
