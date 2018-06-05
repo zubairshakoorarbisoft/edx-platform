6009d8c7a508a792d334d7a97c2f48bf2b227633
Jason Myatt <jmyatt@edx.org>
2018-06-05 16:15:56 -0400

    fix model field names

--- openedx/core/djangoapps/assignments/models.py
+++ openedx/core/djangoapps/assignments/models.py
@@ -9,15 +9,15 @@ class CourseRunAssignment(models.Model):
     CourseRunAssignment model.
     """
     course_run_id = models.CharField(max_length=100, null=False, blank=False)
-    sequential_id = models.CharField(max_length=100, null=False, blank=False)
-    sequential_name = models.CharField(max_length=255, null=False, blank=False)
-    due_date = models.DateTimeField(auto_now_add=True)
+    block_id = models.CharField(max_length=100, null=False, blank=False)
+    display_name = models.CharField(max_length=255, null=False, blank=False)
+    due_date = models.DateTimeField(null=True, blank=True)
 
     class Meta(object):
         """
         CourseRunAssignment metadata.
         """
-        unique_together = ('course_run_id', 'sequential_id')
+        unique_together = ('course_run_id', 'block_id')
 
     def __unicode__(self):
-        return self.sequential_name
+        return self.display_name
