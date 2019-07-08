08cf27de42c016da243b1577d9369d4135e4f90d
Stuart Young <syoung@edx.org>
2019-07-08 16:29:19 -0400

    add script to create annotation report for feature toggles

---
+++ scripts/generate-feature-toggle-annotation-report.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# generate-feature-toggle-annotation-report.sh
+
+# Generate a code annotations report for feature toggles in the platform.
+# This script is part of the feature toggle generator tool.
+
+pip install -r requirements/edx/paver.txt -r requirements/edx/testing.txt
+paver install_python_prereqs
+rm -Rf reports/*
+code_annotations static_find_annotations --config_file=$CODE_ANNOTATION_CONFIG_PATH --no_lint
+mkdir -p $CODE_ANNOTATION_OUTPUT_PATH
+cp reports/* $CODE_ANNOTATION_OUTPUT_PATH/lms-annotations.yml
