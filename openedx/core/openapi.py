4dab8673e3bb615400c86d4b338cb930c816bbe9
Ned Batchelder <ned@edx.org>
2019-07-18 15:26:04 -0400

    Functions to support OpenAPI docs in decorators

--- openedx/core/openapi.py
+++ openedx/core/openapi.py
@@ -2,9 +2,12 @@
 Open API support.
 """
 
+import textwrap
+
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema as drf_swagger_auto_schema
 
 openapi_info = openapi.Info(
     title="Open edX API",
@@ -20,3 +23,11 @@ schema_view = get_schema_view(
     public=True,
     permission_classes=(permissions.AllowAny,),
 )
+
+
+def swagger_auto_schema(**kwargs):
+    if 'operation_description' in kwargs:
+        kwargs['operation_description'] = textwrap.dedent(kwargs['operation_description'])
+    for param in kwargs.get('manual_parameters', ()):
+        param.description = textwrap.dedent(param.description)
+    return drf_swagger_auto_schema(**kwargs)
