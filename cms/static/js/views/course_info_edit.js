d62bbb0a4aca2da7e741b6ce336427fd645a1c74
Feanil Patel <feanil@edx.org>
2019-07-23 21:28:28 -0400

    fixup! Remove parse_rest.

--- cms/static/js/views/course_info_edit.js
+++ cms/static/js/views/course_info_edit.js
@@ -15,7 +15,7 @@ define(['js/views/baseview', 'js/views/course_info_update', 'js/views/course_inf
                 new CourseInfoUpdateView({
                     el: $('body.updates'),
                     collection: this.model.get('updates'),
-                    base_asset_url: this.model.get('base_asset_url'),
+                    base_asset_url: this.model.get('base_asset_url')
                 });
 
                 new CourseInfoHandoutView({
