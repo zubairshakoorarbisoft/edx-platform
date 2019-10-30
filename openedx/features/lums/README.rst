42b300e292e517cbfca7457a32bf62642fd0144d
zubair-arbi <zubair.afzal@arbisoft.com>
2019-10-30 11:35:58 +0500

    fix typos in lums feature app

--- openedx/features/lums/README.rst
+++ openedx/features/lums/README.rst
@@ -2,4 +2,4 @@ Lums
 -----
 
 This directory contains a Django application to add custom functionality for
-lums features within edx-platform.
+LUMS SeO features within edx-platform.
--- openedx/features/lums/admin.py
+++ openedx/features/lums/admin.py
@@ -4,7 +4,9 @@ from openedx.features.lums.models import UserProfileExtension
 
 
 class UserProfileExtensionAdmin(admin.ModelAdmin):
-    """ Admin interface for the UserAttribute model. """
+    """
+    Admin interface for the UserProfileExtension model.
+    """
     list_display = ('user', 'province',)
     list_filter = ('province',)
     search_fields = ('user__username', 'province',)
