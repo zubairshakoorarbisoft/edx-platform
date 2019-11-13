77e6605bfaa04bbf089829bf34df8ccc7f387cc1
iamhassantariq <23108499+imhassantariq@users.noreply.github.com@users.noreply.github.com>
2019-11-13 17:00:22 +0500

    Checking with tox

--- pavelib/tests.py
+++ pavelib/tests.py
@@ -49,7 +49,7 @@ __test__ = False  # do not collect
     make_option('--no-randomize', action='store_false', dest='randomize', help="don't run the tests in a random order"),
     make_option("--verbose", action="store_const", const=2, dest="verbosity"),
     make_option("-q", "--quiet", action="store_const", const=0, dest="verbosity"),
-    make_option("-v", "--verbosity", action="count", dest="verbosity", default=1),
+    make_option("-v", "--verbosity", action="count", dest="verbosity", default=3),
     make_option(
         "--disable_capture", action="store_true", dest="disable_capture",
         help="Disable capturing of stdout/stderr"
