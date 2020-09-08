671f313e1ab15e88a4ee5ad9370dd2f9258f1acc
M Umar Khan <umar.khan@arbisoft.com>
2020-09-08 15:14:29 +0500

    Add azuread b2c backend

--- openedx/features/clearesult_features/auth_backend.py
+++ openedx/features/clearesult_features/auth_backend.py
@@ -3,7 +3,7 @@ Custom auth backend for Clearesult.
 """
 import logging
 
-from social_core.backends.azuread import AzureADOAuth2
+from social_core.backends.azuread_b2c import AzureADB2COAuth2
 from social_core.exceptions import AuthException
 
 from openedx.core.djangoapps.theming.helpers import get_current_request
@@ -11,11 +11,15 @@ from openedx.core.djangoapps.theming.helpers import get_current_request
 LOGGER = logging.getLogger(__name__)
 
 
-class ClearesultAzureADOAuth2(AzureADOAuth2):
+class ClearesultAzureADOAuth2(AzureADB2COAuth2):
     """
     Custom OAuth2 backend for Clearesult.
     """
     name = 'clearesult-azuread-oauth2'
+
+    @property
+    def base_url(self):
+        return self.setting('BASE_URL').format(policy=self.policy)
 
     def get_redirect_uri(self, state=None):
         """
