83a66337ef85022eff5819e5f2c1ce850303d850
mumerkhan999 <umar.khan@arbisoft.com>
2020-12-17 16:31:52 +0500

    Add catalogs_manager functionaliy: API and React Pages

--- openedx/features/clearesult_features/api/v0/views.py
+++ openedx/features/clearesult_features/api/v0/views.py
@@ -1,16 +1,35 @@
 """
 Views for Clearesult V0 APIs
 """
+from django.contrib.sites.models import Site
+from django.db import IntegrityError
+from django.db.models import Q
+from django.shortcuts import get_object_or_404
 from rest_framework import generics
 from rest_framework import permissions
 from rest_framework import viewsets
-from rest_framework.authentication import SessionAuthentication
+from rest_framework import status
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.exceptions import NotFound
+from rest_framework.response import Response
 
 from edx_rest_framework_extensions.auth.jwt.authentication import JwtAuthentication
 
-from openedx.features.clearesult_features.models import ClearesultCreditProvider, UserCreditsProfile
-from openedx.features.clearesult_features.api.v0.serializers import UserCreditsProfileSerializer, ClearesultCreditProviderSerializer
-
+from openedx.core.lib.api.authentication import BearerAuthentication
+from openedx.features.clearesult_features.models import (
+    ClearesultCreditProvider, UserCreditsProfile, ClearesultCatalog,
+    ClearesultCourse
+)
+from openedx.features.clearesult_features.api.v0.serializers import (
+    UserCreditsProfileSerializer, ClearesultCreditProviderSerializer,
+    ClearesultCatalogSerializer, ClearesultCourseSerializer,
+    SiteSerializer
+)
+from openedx.features.clearesult_features.models import ClearesultLocalAdmin
+from openedx.features.clearesult_features.api.v0.validators import (
+    validate_data_for_catalog_creation, validate_data_for_catalog_updation, validate_clearesult_catalog_pk,
+    validate_sites_for_local_admin, validate_catalog_update_deletion
+)
 
 class IsSelf(permissions.BasePermission):
 
@@ -32,8 +51,22 @@ class IsSelf(permissions.BasePermission):
         return profile.user == current_user
 
 
+class IsAdminOrLocalAdmin(permissions.BasePermission):
+
+    message = 'You are not authorized to perform this action.'
+
+    def has_permission(self, request, view):
+        if request.user.is_superuser:
+            return True
+
+        if ClearesultLocalAdmin.objects.filter(user=request.user):
+            return True
+
+        return False
+
+
 class UserCreditProfileViewset(viewsets.ModelViewSet):
-    authentication_classes = (JwtAuthentication, SessionAuthentication,)
+    authentication_classes = (SessionAuthentication, JwtAuthentication, )
     serializer_class = UserCreditsProfileSerializer
     pagination_class = None
 
@@ -81,3 +114,261 @@ class ClearesultCredeitProviderListView(generics.ListAPIView):
     permission_classes = (permissions.IsAuthenticated, )
     serializer_class = ClearesultCreditProviderSerializer
     pagination_class = None
+
+
+class ClearesultCatalogViewset(viewsets.ViewSet):
+    authentication_classes = [BasicAuthentication, SessionAuthentication, BearerAuthentication, JwtAuthentication]
+    permission_classes = [permissions.IsAuthenticated, IsAdminOrLocalAdmin]
+
+    def list(self, request):
+        """
+        Return the list of Clearesult Catalogs in the following form
+        GET /clearesult/api/v0/catalogs/
+
+        [
+            {
+                "id": 39
+                "name": "Dummy catalog"
+                "site": {
+                    "id": 1,
+                    "domain": "localhost:18000"
+                },
+                "clearesult_courses": [
+                    {
+                        "course_name": "Testing automata course",
+                        "course_id": "course-v1:edx+22+2020_T122",
+                        "site": {
+                            "id": 1,
+                            "domain": "localhost:18000"
+                        }
+                    },
+                    ...
+                    ...
+                ]
+            },
+            ...
+            ...
+        ]
+
+        "site": null means that course or catalog is public
+        """
+        queryset = ClearesultCatalog.objects.select_related('site').prefetch_related('clearesult_courses')
+        if not request.user.is_superuser:
+            error, allowed_sites = validate_sites_for_local_admin(request.user)
+            if error:
+                return error
+            queryset = queryset.filter(Q(site__in=allowed_sites) | Q(site=None))
+
+        serializer = ClearesultCatalogSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def create(self, request):
+        """
+        Create a new catalog and accept data in the following format
+        POST /clearesult/api/v0/catalogs/
+
+        {
+            "name": "Dummy catalog name",
+            "site": {
+                "domain": "localhost:18000"
+            },
+            "clearesult_courses": [
+                "course-v1:edx+22+2020_T122",
+                "course-v1:edx+22+2020_T123",
+                "course-v1:edx+22+2020_T124"
+            ]
+        }
+
+
+        "site": {} means that catalog will be public
+        """
+        error, allowed_sites = validate_sites_for_local_admin(request.user)
+        if error:
+            return error
+
+        data = request.data
+        error, validated_data = validate_data_for_catalog_creation(data, request.user, allowed_sites)
+        if error:
+            return error
+
+        name = validated_data.get('name')
+        site = validated_data.get('site')
+        courses = validated_data.get('clearesult_courses')
+
+        if not request.user.is_superuser:
+            try:
+                ClearesultLocalAdmin.objects.get(user=request.user, site=site)
+            except ClearesultLocalAdmin.DoesNotExist:
+                return Response(
+                    {'detail': 'You are not allowed to perform this action'},
+                    status=status.HTTP_403_FORBIDDEN
+                )
+
+        try:
+            instance = ClearesultCatalog.objects.create(name=name, site=site)
+        except IntegrityError:
+            return Response(
+                {'detail': 'A clearesult catalog with this name and site already exists.'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        if len(courses) > 0:
+            instance.clearesult_courses.add(*courses)
+
+        serializer = ClearesultCatalogSerializer(instance)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        """
+        Return a single instance of catalog as per pk
+        GET /clearesult/api/v0/catalogs/1
+        """
+        queryset = ClearesultCatalog.objects.select_related('site').prefetch_related('clearesult_courses')
+        if not request.user.is_superuser:
+            error, allowed_sites = validate_sites_for_local_admin(request.user)
+            if error:
+                return error
+            queryset = queryset.filter(Q(site__in=allowed_sites) | Q(site=None))
+
+        data = get_object_or_404(queryset, pk=pk)
+        serializer = ClearesultCatalogSerializer(data)
+        return Response(serializer.data)
+
+    def partial_update(self, request, pk=None):
+        """
+        Partially update the requested catalog
+        PATCH /clearesult/api/v0/catalogs/1
+
+        {
+            "name": "Dummy new name",
+            "clearesult_courses": [
+                "course-v1:edx+22+2020_T122",
+                "course-v1:edx+22+2020_T123"
+            ]
+        }
+
+        not receiving site as site can not be changed.
+        """
+        error, allowed_sites = validate_sites_for_local_admin(request.user)
+        if error:
+            return error
+
+        data = request.data
+        data['pk'] = pk
+        error, validated_data = validate_data_for_catalog_updation(data, request.user, allowed_sites)
+        if error:
+            return error
+
+        name = validated_data.get('name')
+        courses = validated_data.get('clearesult_courses')
+        instance = validated_data.get('clearesult_catalog')
+
+        if not request.user.is_superuser:
+            error = validate_catalog_update_deletion(request.user, instance)
+            if error:
+                return error
+
+        try:
+            instance.name = name
+            instance.save()
+        except IntegrityError:
+            return Response(
+                {'detail': 'A clearesult catalog with this name and site already exists.'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        # updating site is not allowed
+        instance.clearesult_courses.clear()
+        if len(courses) > 0:
+            instance.clearesult_courses.add(*courses)
+        serializer = ClearesultCatalogSerializer(instance)
+        return Response(serializer.data)
+
+    def destroy(self, request, pk=None):
+        """
+        Delete the particular instance of catalog as per pk
+        DELETE /clearesult/api/v0/catalogs/2
+        """
+        error, instance = validate_clearesult_catalog_pk(pk)
+        if error:
+            return error
+
+        if not request.user.is_superuser:
+            error = validate_catalog_update_deletion(request.user, instance)
+            if error:
+                return error
+
+        instance.delete()
+        return Response(
+            {'detail': 'The clearesult catalog with id {} has been deleted successfully.'.format(pk)},
+            status=status.HTTP_200_OK
+        )
+
+
+class ClearesultCourseViewset(viewsets.ViewSet):
+    authentication_classes = [BasicAuthentication, SessionAuthentication, BearerAuthentication, JwtAuthentication,]
+    permission_classes = [permissions.IsAuthenticated, IsAdminOrLocalAdmin]
+
+    def list(self, request):
+        """
+        Return the list of available courses
+        GET /clearesult/api/v0/courses/
+
+        [
+            {
+                "course_name": "Testing automata course",
+                "course_id": "course-v1:edx+22+2020_T122",
+                "site": {
+                    "id": 1,
+                    "domain": "localhost:18000"
+                }
+            },
+            ...
+            ...
+        ]
+
+        """
+        queryset = ClearesultCourse.objects.select_related('site')
+        if not request.user.is_superuser:
+            error, allowed_sites = validate_sites_for_local_admin(request.user)
+            if error:
+                return error
+            queryset = queryset.filter(Q(site__in=allowed_sites) | Q(site=None))
+
+        serializer = ClearesultCourseSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+class SiteViewset(viewsets.ViewSet):
+    authentication_classes = [BasicAuthentication, SessionAuthentication, BearerAuthentication, JwtAuthentication,]
+    permission_classes = [permissions.IsAuthenticated, IsAdminOrLocalAdmin]
+
+    def list(self, request):
+        """
+        Return the list of available sites
+        GET /clearesult/api/v0/sites/
+
+        [
+            {
+                "id": 1,
+                "domain": "localhost:18000",
+            }
+            ...
+            ...
+        ]
+        """
+        if request.user.is_superuser:
+            queryset = Site.objects.filter(name__iendswith='LMS')
+        else:
+            queryset = ClearesultLocalAdmin.objects.filter(
+                user=request.user,
+                site__name__iendswith='LMS'
+            ).values('site')
+            local_sites_ids = []
+            for item in queryset:
+                local_sites_ids.append(item.get('site'))
+
+            queryset = Site.objects.filter(id__in=local_sites_ids)
+
+        serializer = SiteSerializer(queryset, many=True)
+        return Response(serializer.data)
---
+++ openedx/features/clearesult_features/static/catalogs_manager/js/CatalogsContent.js
@@ -0,0 +1,208 @@
+import React, {useState, useEffect} from 'react';
+import { ToastsContainer, ToastsStore } from "react-toasts";
+import Cookies from "js-cookie";
+import Table from './components/Table';
+import AddEditModal from './components/AddEditModal';
+import HttpClient from "../../continuing_education/js/client";
+
+
+
+
+export default function CatalogsContent({context}) {
+    // all states
+    const [availableCatalogs, setAvailableCatalogs] = useState([]);
+    const [isEditButton, setIsEditButton] = useState(false);
+    const [catalogName, setCatalogName] = useState("");
+    const [currentCatalog, setCurrentCatalog] = useState({});
+    // will be used for super user
+    const [isPublicAdded, setIsPublicAdded] = useState(false);
+    const [availableSites, setAvailableSites] = useState([]);
+    const [selectedSite, setSelectedSite] = useState({});
+    // will fetch the all available courses in one API call
+    // store them in availableCourses
+    const [availableCourses, setAvailableCourses] = useState([]);
+    // if user apply some filteration on available courses
+    // store the filtered result in availableFilteredCourses
+    const [availableFilteredCourses, setAvailableFilteredCourses] = useState([]);
+    const [selectedCourses, setSelectedCourses] = useState([]);
+
+    // http client to perform API requests
+    const client = new HttpClient({
+        headers: {
+            "X-CSRFToken": Cookies.get("csrftoken"),
+        },
+    });
+
+    // all the fucntions performing API calls
+    const getAvailableCatalogs = async () => {
+        let data = (await client.get(context.CATALOGS_URL)).data;
+        setAvailableCatalogs(data);
+    }
+
+    const getAvailableSites = async () => {
+        let data = (await client.get(context.SITES_URL)).data;
+        setAvailableSites(data);
+    }
+
+    const getAvailableCourses = async () => {
+        let data = (await client.get(context.COURSES_URL)).data;
+        setAvailableCourses(data);
+    }
+
+    const createOrUpdateCatalog = async (params) => {
+        if (!isEditButton) {
+            if (params.site.id == 0) {
+                params.site = {};
+            }
+            try {
+                let responseData = (await client.post(context.CATALOGS_URL, params)).data;
+                setAvailableCatalogs([...availableCatalogs, responseData]);
+                setCurrentCatalog(responseData);
+                ToastsStore.success('Success: Catalog has been added.');
+                setIsEditButton(true);
+            } catch (ex) {
+                ToastsStore.error('Failed: Failed to add catalog.');
+            }
+        } else {
+            try {
+                let responseData = (await client.patch(context.CATALOGS_URL + currentCatalog.id.toString(), params)).data;
+                setCurrentCatalog(responseData);
+                const updatedCatalogs = availableCatalogs.filter((availableCatalog) => availableCatalog.id != currentCatalog.id)
+                setAvailableCatalogs([...updatedCatalogs, responseData])
+                ToastsStore.success('Updated: Catalog has been updated.');
+            } catch (ex) {
+                ToastsStore.error('Failed: Failure while updating .');
+            }
+        }
+    }
+
+    const handleDeleteCatalogButton = async (event) => {
+        let pk = event.target.value;
+        if (confirm('Are you sure to delete the catalog with id: ' + pk)) {
+            try {
+                let data = (await client.delete(context.CATALOGS_URL + pk)).data
+                const newCatalogs = availableCatalogs.filter((availableCatalog) => availableCatalog.id != pk);
+                setAvailableCatalogs(newCatalogs);
+                ToastsStore.success("Success: The requested catalog has been deleted successfully.");
+            } catch (ex) {
+                ToastsStore.error('Failed: Could not delete the requested catalog.');
+            }
+        }
+    }
+
+    const loadInitialData = () => {
+        getAvailableCatalogs();
+        getAvailableSites();
+        getAvailableCourses();
+    }
+
+    useEffect(() => {
+        loadInitialData();
+    }, [])
+
+
+    const handleAddCatalogButton = (event) => {
+        // perform the necessary fetching
+        if (!isPublicAdded && context.IS_SUPERUSER == 1) {
+            setAvailableSites([...availableSites, { id: 0, domain: "public"}])
+            setIsPublicAdded(true);
+        }
+        setCatalogName("");
+        setIsEditButton(false);
+        // by default for super user the site will be public
+        if (context.IS_SUPERUSER == 1) {
+            setSelectedSite({id:0, domain:"public"});
+            filterCoursesPerSelectedSite(0);
+        } else {
+            if (availableSites.length > 0) {
+                setSelectedSite(availableSites[0]);
+                filterCoursesPerSelectedSite(availableSites[0].id);
+            }
+        }
+        setSelectedCourses([]);
+    }
+
+    const handleEditCatalogButton = (event) => {
+        // perform the necessary fetching
+        // will be sent in table
+        let catalog = availableCatalogs.filter((availableCatalog) => availableCatalog.id == event.target.value)[0];
+        setCurrentCatalog(catalog);
+        if (catalog.site == null) {
+            setSelectedSite({id: 0, domain:"public"});
+            filterCoursesPerSelectedSite(0);
+        } else {
+            setSelectedSite(catalog.site);
+            filterCoursesPerSelectedSite(catalog.site.id);
+        }
+        setCatalogName(catalog.name);
+        setSelectedCourses(catalog.clearesult_courses.map(clearesult_course => clearesult_course.course_id));
+        setIsEditButton(true);
+    }
+
+    const filterCoursesPerSelectedSite = (siteId) => {
+        var filteredData = availableCourses.filter((availableCourse) => (availableCourse.site == null || (availableCourse.site.id == siteId)))
+        setAvailableFilteredCourses(filteredData);
+    }
+
+    const handleSiteSelect = (event) => {
+        // now you have selected site
+        // and all available valid courses
+        // and now filter out courses from these courses
+        // using that site
+        setSelectedSite({
+            id: parseInt(event.target.value),
+            domain: event.target.options[event.target.selectedIndex].text
+        });
+        filterCoursesPerSelectedSite(event.target.value);
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // now you have all the values you can perform the
+        // update / create stuff based on the value of isEditButton
+
+        // dual list box will don't do anything with the availableCourses
+        // it will remain as it is but make sure that you update the value of
+        // selectedCourse
+        let params = {
+            "name": catalogName,
+            "clearesult_courses": selectedCourses,
+            "site": selectedSite
+        }
+        createOrUpdateCatalog(params);
+    }
+
+    return (
+        <div>
+            <button
+                type="button"
+                className="btn btn-primary"
+                data-toggle="modal"
+                data-target="#exampleModalCenter"
+                onClick={handleAddCatalogButton}
+            >
+                New
+            </button>
+
+            <AddEditModal
+                catalogName={catalogName}
+                availableSites={availableSites}
+                availableFilteredCourses={availableFilteredCourses}
+                selectedCourses={selectedCourses}
+                handleSubmit={handleSubmit}
+                isEditButton={isEditButton}
+                setCatalogName={setCatalogName}
+                handleSiteSelect={handleSiteSelect}
+                setSelectedCourses={setSelectedCourses}
+                selectedSite={selectedSite}
+            />
+            <Table
+                availableCatalogs={availableCatalogs}
+                handleDeleteCatalogButton={handleDeleteCatalogButton}
+                handleEditCatalogButton={handleEditCatalogButton}
+                isSuperUser={context.IS_SUPERUSER}
+            />
+            <ToastsContainer store={ToastsStore} />
+        </div>
+    )
+}
