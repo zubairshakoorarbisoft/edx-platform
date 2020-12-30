bcf1021d48fae546e7d62d7b5125c4cfe6460f57
Rehan <rehan99000@gmail.com>
2020-12-30 18:24:18 +0500

    added logs and passed disable_emails from signal

--- cms/djangoapps/cms_user_tasks/signals.py
+++ cms/djangoapps/cms_user_tasks/signals.py
@@ -3,8 +3,11 @@ Receivers of signals sent from django-user-tasks
 """
 from __future__ import absolute_import, print_function, unicode_literals
 
+from crum import CurrentRequestUserMiddleware
 import logging
+from django.conf import settings
 
+from openedx.core.djangoapps.site_configuration import helpers as configuration_helpers
 from django.urls import reverse
 from django.dispatch import receiver
 from user_tasks.models import UserTaskArtifact
@@ -50,13 +53,20 @@ def user_task_stopped_handler(sender, **kwargs):  # pylint: disable=unused-argum
             )
 
         try:
+            disable_emails = configuration_helpers.get_value(
+                'DISABLE_CMS_TASK_EMAILS',
+                settings.FEATURES.get('DISABLE_CMS_TASK_EMAILS', True)
+            )
+            LOGGER.info('Settings Features: {}'.format(disable_emails))
+            LOGGER.info('from address: {}'.format(configuration_helpers.get_value('email_from_address')))
+            LOGGER.info('configurations enabled: {}'.format(configuration_helpers.is_site_configuration_enabled()))
             # Need to str state_text here because it is a proxy object and won't serialize correctly
-            with emulate_http_request():
-                send_task_complete_email.delay(
-                    status.name.lower(),
-                    str(status.state_text),
-                    status.user.email,
-                    detail_url,
-                )
+            send_task_complete_email.delay(
+                status.name.lower(),
+                str(status.state_text),
+                status.user.email,
+                detail_url,
+                disable_emails,
+            )
         except Exception:  # pylint: disable=broad-except
             LOGGER.exception("Unable to queue send_task_complete_email")
