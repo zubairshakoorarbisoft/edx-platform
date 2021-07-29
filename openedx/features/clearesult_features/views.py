890a3cb933cc6a27243ca34469213892af9f6c34
M Umar Khan <umar.khan@arbisoft.com>
2021-07-29 20:56:16 +0500

    Add queryparam for continuing page

--- openedx/features/clearesult_features/views.py
+++ openedx/features/clearesult_features/views.py
@@ -6,6 +6,7 @@ from openedx.features.clearesult_features.authentication.permissions import loca
 
 @login_required
 def render_continuing_education(request):
+    request.user.clearesult_profile.set_extension_value('has_visited_continuing_education_form', True)
     return render_to_response('clearesult/continuing_education.html', {'uses_bootstrap': True,})
 
 
@@ -17,6 +18,7 @@ def render_reports(request):
 
 @login_required
 def render_participation_code(request):
+    request.user.clearesult_profile.set_extension_value('has_visited_participation_code', True)
     return render_to_response('clearesult/participation_code.html', {'uses_bootstrap': True,})
 
 
