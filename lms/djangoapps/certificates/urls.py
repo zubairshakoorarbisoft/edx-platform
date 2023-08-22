b39d00471118cfbd3f9ee0b800bc03f5574a3bd8
Muhammad Farhan <muhammad.farhan@arbisoft.com>
2023-08-22 16:51:43 +0500

    fix: test2

--- lms/djangoapps/certificates/urls.py
+++ lms/djangoapps/certificates/urls.py
@@ -36,5 +36,4 @@ urlpatterns = [
     url(r'^search', views.search_certificates, name="search"),
     url(r'^regenerate', views.regenerate_certificate_for_user, name="regenerate_certificate_for_user"),
     url(r'^generate', views.generate_certificate_for_user, name="generate_certificate_for_user"),
-    url(r'^qwer', views.qwer)
 ]
