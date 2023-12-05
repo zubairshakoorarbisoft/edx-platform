e6ca6821a8cd7f2b17cc068cc048040299ee41a6
daniyal.farman <daniyal.farman@arbisoft.com>
2023-12-05 21:50:37 +0500

    testing

--- openedx/features/edly/urls.py
+++ openedx/features/edly/urls.py
@@ -5,6 +5,7 @@ URLs Edly app views.
 from django.conf.urls import url
 
 from openedx.features.edly.views import account_deactivated_view
+app_name = 'edly_app_urls'
 
 urlpatterns = [
     url('account_deactivated/', account_deactivated_view, name='account_deactivated_view'),
