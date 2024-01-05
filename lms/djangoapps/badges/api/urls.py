fce2dc0018b9340df6a11eaa2ba139c5bb835cb7
Muhammad Ali <muhammad.ali@arbisoft.com>
2024-01-06 02:00:05 +0500

    Added API to verify LMS token

--- lms/djangoapps/badges/api/urls.py
+++ lms/djangoapps/badges/api/urls.py
@@ -6,11 +6,12 @@ URLs for badges API
 from django.conf import settings
 from django.urls import re_path
 
-from .views import UserBadgeAssertions, LeaderboardView
+from .views import UserBadgeAssertions, LeaderboardView, VerfyTokenView
 
 urlpatterns = [
     re_path('^assertions/user/' + settings.USERNAME_PATTERN + '/$',
             UserBadgeAssertions.as_view(), name='user_assertions'),
     
-    re_path('leaderboard/', LeaderboardView.as_view(), name='leaderboard')
+    re_path('leaderboard/', LeaderboardView.as_view(), name='leaderboard'),
+    re_path('verify-lms-token/', VerfyTokenView.as_view(), name='verify-lms-token'),
 ]
