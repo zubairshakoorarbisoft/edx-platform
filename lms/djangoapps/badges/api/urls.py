90213f4414d8fc3c939c067f72b82e1c5e5ebdda
Ali Salman <ali.salman@A006-01181.local>
2024-04-16 10:23:28 +0500

    update: add user-score on the leaderbaord

--- lms/djangoapps/badges/api/urls.py
+++ lms/djangoapps/badges/api/urls.py
@@ -1,17 +1,19 @@
 """
 URLs for badges API
 """
-
-
 from django.conf import settings
-from django.urls import re_path
+from django.urls import re_path, path, include
 
 from .views import UserBadgeAssertions, LeaderboardView, VerfyTokenView
 
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'leaderboard', LeaderboardView, basename='leaderboard')
+
 urlpatterns = [
+    path('', include(router.urls)),
     re_path('^assertions/user/' + settings.USERNAME_PATTERN + '/$',
             UserBadgeAssertions.as_view(), name='user_assertions'),
-    
-    re_path('leaderboard/', LeaderboardView.as_view(), name='leaderboard'),
     re_path('verify-lms-token/', VerfyTokenView.as_view(), name='verify-lms-token'),
 ]
