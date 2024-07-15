0de6792721c0624f3f73072b8c79b544d168495c
Muhammad Faraz  Maqsood <faraz.maqsood@A006-01130.local>
2024-07-15 14:09:52 +0500

    feat: add new APIs for stats

--- openedx/features/sdaia_features/course_progress/api/v1/views.py
+++ openedx/features/sdaia_features/course_progress/api/v1/views.py
@@ -7,11 +7,13 @@ import requests
 
 from ccx_keys.locator import CCXLocator
 from django.conf import settings
+from django.contrib.auth.models import User
 from django.utils.decorators import method_decorator
 from edx_rest_framework_extensions.auth.jwt.authentication import JwtAuthentication
 from rest_framework import permissions, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
 
 from common.djangoapps.student.roles import (
     CourseInstructorRole,
@@ -19,6 +21,11 @@ from common.djangoapps.student.roles import (
     UserBasedRole,
 )
 from common.djangoapps.util.disable_rate_limit import can_disable_rate_limit
+from edx_rest_framework_extensions.auth.session.authentication import (
+    SessionAuthenticationAllowInactiveUser,
+)
+from lms.djangoapps.badges.models import LeaderboardEntry
+from lms.djangoapps.certificates.models import GeneratedCertificate
 from openedx.core.djangoapps.cors_csrf.decorators import ensure_csrf_cookie_cross_domain
 from openedx.core.djangoapps.enrollments.errors import CourseEnrollmentError
 from openedx.core.djangoapps.enrollments.data import get_course_enrollments
@@ -26,6 +33,9 @@ from openedx.core.djangoapps.enrollments.views import EnrollmentCrossDomainSessi
 from openedx.core.djangoapps.programs.utils import ProgramProgressMeter
 from openedx.core.lib.api.authentication import BearerAuthenticationAllowInactiveUser
 from openedx.core.lib.api.permissions import ApiKeyHeaderPermissionIsAuthenticated
+from openedx.features.sdaia_features.course_progress.utils import (
+    get_user_certificates,
+)
 
 
 log = logging.getLogger(__name__)
@@ -34,16 +44,17 @@ log = logging.getLogger(__name__)
 @can_disable_rate_limit
 class UserStatsAPIView(APIView):
     """
-    APIView to get the total watch hours for a user.
+    APIView to get the user stats.
 
     **Example Requests**
         GET /sdaia/api/v1/user_stats
 
-        It return watch_time in hours
         Response: {
             "watch_hours": 0.00043390860160191856,
             "enrolled_courses": enrolled_courses,
             "enrolled_programs": enrolled_programs,
+            "user_certificates": user_certificates,
+            "score": score,
         }
     """
 
@@ -57,7 +68,7 @@ class UserStatsAPIView(APIView):
     @method_decorator(ensure_csrf_cookie_cross_domain)
     def get(self, request):
         """
-        Gets the total watch hours for a user.
+        Gets the stats for a user.
         """
         user = request.user
         user_id = user.id
@@ -94,6 +105,13 @@ class UserStatsAPIView(APIView):
                 },
             )
 
+        ############ USER CERTIFICATES ############
+        user_certificates = get_user_certificates(username)
+
+        ############ USER SCORE ############
+        leaderboard = LeaderboardEntry.objects.filter(user=user)
+        score = leaderboard and leaderboard.first().score
+
         ############ Response ############
         return Response(
             status=status.HTTP_200_OK,
@@ -101,5 +119,62 @@ class UserStatsAPIView(APIView):
                 "watch_hours": watch_time,
                 "enrolled_courses": enrolled_courses,
                 "enrolled_programs": no_of_programs,
+                "user_certificates": user_certificates,
+                "score": score,
+            },
+        )
+
+
+@can_disable_rate_limit
+class DashboardStatsAPIView(APIView):
+    """
+    APIView to get the dashboard stats.
+
+    **Example Requests**
+        GET /sdaia/api/v1/dashboard_stats
+
+        Response: {
+
+        }
+    """
+
+    authentication_classes = (
+        JwtAuthentication,
+        BearerAuthenticationAllowInactiveUser,
+        SessionAuthenticationAllowInactiveUser,
+    )
+    permission_classes = (IsAuthenticated,)
+
+    @method_decorator(ensure_csrf_cookie_cross_domain)
+    def get(self, request):
+        """
+        Gets the stats for dashboard.
+        """
+        user = request.user
+        users_count = User.objects.all().count()
+        certificates_count = GeneratedCertificate.objects.all().count()
+        clickhouse_uri = (
+            f"{settings.CAIRN_CLICKHOUSE_HTTP_SCHEME}://{settings.CAIRN_CLICKHOUSE_USERNAME}:{settings.CAIRN_CLICKHOUSE_PASSWORD}@"
+            f"{settings.CAIRN_CLICKHOUSE_HOST}:{settings.CAIRN_CLICKHOUSE_HTTP_PORT}/?database={settings.CAIRN_CLICKHOUSE_DATABASE}"
+        )
+        query = f"SELECT SUM(duration) as `Watch time` FROM `openedx`.`video_view_segments`;"
+
+        ############ TOTAL WATCH HOURS ############
+        try:
+            response = requests.get(clickhouse_uri, data=query.encode("utf8"))
+            watch_time = float(response.content.decode().strip()) / (60 * 60)
+        except Exception as e:
+            log.error(
+                f"Unable to fetch total watch hours due to this exception: {str(e)}"
+            )
+            raise HTTPException(status_code=500, detail=str(e))
+
+        ############ Response ############
+        return Response(
+            status=status.HTTP_200_OK,
+            data={
+                "users_count": users_count,
+                "certificates_count": certificates_count,
+                "total_watch_time": watch_time,
             },
         )
