20521c0d55763b657e5e8b3134d4862339ad253e
Muhammad Faraz  Maqsood <faraz.maqsood@A006-01130.local>
2024-07-15 15:39:39 +0500

    feat: add new APIs for stats

--- openedx/features/sdaia_features/course_progress/api/v1/urls.py
+++ openedx/features/sdaia_features/course_progress/api/v1/urls.py
@@ -3,11 +3,12 @@ URLs for User Watch Hours - SDAIA Specific.
 """
 
 from django.urls import path  # pylint: disable=unused-import
-from .views import UserStatsAPIView
+from .views import UserStatsAPIView, DashboardStatsAPIView
 
 
 app_name = "nafath_api_v1"
 
 urlpatterns = [
     path(r"user_stats", UserStatsAPIView.as_view()),
+    path(r"dashboard_stats", DashboardStatsAPIView.as_view()),
 ]
